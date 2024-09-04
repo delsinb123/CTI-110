bdb48e4d4227163cfe1bdd27b799f5d06c367763
delsinb123 <delsinbarrett@gmail.com>
2024-09-04 18:33:55 +0000

    Change in file

---
+++ P1HW1_BarrettDelsin.py
@@ -0,0 +1,25 @@
+# Delsin Barrett
+# 9/4/2024
+# P1LAB1
+# Input and Output with Integers
+
+print("----Calculating Exponents----")
+print()
+print()
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(base_value, "raised to the power of", exponent, "is", base_value**exponent, "!!")
+print()
+print()
+
+print("----Addition and Subtraction----")
+print()
+print()
+starting_integer = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(starting_integer, "+", add, "-", subtract, "is equal to", starting_integer+add-subtract)
