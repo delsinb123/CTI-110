cb7cb5052f2fa0682bfd409993ee702f8a176206
delsinb123 <delsinbarrett@gmail.com>
2024-09-04 17:58:32 +0000

    Change the file

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+# Delsin Barrett
+# 9/4/2024
+# P1LAB1
+# Input and Output
+
+#The input function always sves the data as a string by default.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print()
+print("Hello", first_name, last_name + "!", "Welcome to CTI-110")
+print()
