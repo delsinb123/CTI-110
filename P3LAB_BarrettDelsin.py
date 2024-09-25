7bc28ec03da4948428654cc7ae5c9569bdea57a0
delsinb123 <delsinbarrett@gmail.com>
2024-09-25 13:52:44 -0400

    Update P3LAB_BarrettDelsin.py

--- P3LAB_BarrettDelsin.py
+++ P3LAB_BarrettDelsin.py
@@ -1,16 +1,62 @@
 # Delsin Barrett
 # 9/4/2024
-# P3HW2
-# Calculate coins needed for change
+# P3LAB
+# Calculate amount of coins needed
+
+#Get info from user
+change = float(input("Enter an amount of money: $"))
+
+print(f"Change Amount: {change}")
+
+#Converting the value to an integer
+change = int(round(change * 100))
+
+#Determine how many coins needed
+num_dollars = change // 100
+change = change - (num_dollars *100)
+
+num_quarters = change // 25
+change = change - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change
+
+if num_dollars > 0:
+    if num_dollars == 1:
+        print(f"{num_dollars} Dollar")
+    else:
+        print(f"{num_dollars} Dollars")
+
+if num_dollars > 0:
+    if num_quarters == 1:
+        print(f"{num_quarters} Quarter")
+    else:
+        print(f"{num_quarters} Quarters")
+
+if num_dimes > 0:
+    if num_dimes == 1:
+        print(f"{num_dimes} Dime")
+    else:
+        print(f"{num_dimes} Dimes")
+
+if num_nickels > 0:
+    if num_dimes == 1:
+        print(f"{num_nickels} Nickel")
+    else:
+        print(f"{num_nickels} Nickels")
+
+if num_pennies > 0:
+    if num_pennies == 1:
+        print(f"{num_pennies} Penny")
+    else:
+        print(f"{num_pennies} Pennies")
+        
 
 
-#Floor Division // Gives you whole number from division
 
-#Modulus % Gives you back the remainder from division
-
-#Get Amount of moeny
-amount = float(input("Enter the amount of money as a float: "))
-
-#Convert value into integer. Last 2 digits are still change
-int_amount = int(round(amount * 100))
-print(int_amount)
+        
